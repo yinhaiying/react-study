0bca893d9e3c647ee16e97847fa14e08e06fc20e
yinhaiying <yinhaiying@58.com>
2020-06-03 20:59:53 +0800

    react实现一个三字棋

--- src/index.js
+++ src/index.js
@@ -19,25 +19,50 @@ const Cell = function(props){
 
     return (
         // <div class = "cell">cell</div>   // 注意,我们这里是在写js，不是写HTML。在js中需要使用className代替class()
-        <div className = "cell" >
+        <div className = "cell" onClick = {props.onClickCell}>
             {props.text}
         </div>
     )
 }
 
-const cellsMap = [
-    [null,null,null],
-    [null,null,null],
-    [null,null,null]
-];
+// const cellsMap = [
+//     [null,null,null],
+//     [null,null,null],
+//     [null,null,null]
+// ];
 
 const Chessboard = function(){
+    // 读取数据使用cellsMap,修改数据使用setCellsMap,useState中是初始值。
+    const [cellsMap,setCellsMap] = React.useState([
+        [null,null,null],
+        [null,null,null],
+        [null,null,null]
+    ]);
+    const [n,setN] = React.useState(0);
+    const tell = function(){
+        console.log('tell');
+    }
+    // 改变CellsMap
+    const onClickCell = (row,col) => {
+       console.log('行:' + row);
+       console.log('列：'+ col);
+       //    如果我们没有改变数组的地址，那么直接set无法生效。
+       let copy = JSON.parse(JSON.stringify(cellsMap));
+       copy[row][col] = n % 2 == 0 ? 'X' : 'O';
+       setCellsMap(copy);
+       setN(n+1);
+    //    判断胜负
+       tell();
+    }
+
     return (
         <div>
+            <div>n:{n}</div>
+            {/* 生成多个Cell组件 */}
             {
-                cellsMap.map(items => <div className = "row">{
-                    items.map(item => <div className = "cell">
-                        <Cell text = {item} />
+                cellsMap.map((items,row) => <div className = "row">
+                    { items.map((item,col) => <div className = "cell">
+                        <Cell text = {item} onClickCell = {() => onClickCell(row,col)} />
                     </div>)
                 }</div>)
             }
