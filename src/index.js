ff1ec53cfb114c1051bf144b6a9770fc546b1131
yinhaiying <yinhaiying@58.com>
2020-06-05 11:17:22 +0800

    react事件

--- src/index.js
+++ src/index.js
@@ -1,58 +1,29 @@
-//  react状态：相当于vue中的data
+//  react事件
 
 import React from 'react';
 import ReactDom from 'react-dom';
-import "./style.css"
 
-// 在父元素中使用state去控制子元素props，从而达到父元素数据传递给子元素。
 
 class ParentCom extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      title:''
+    constructor(props){
+        super(props)
     }
-  }
-  render(){
-    return (
-      <div>
-        <h1>子元素传递给父元素的数据：{this.state.title}</h1>
-        <ChildCom setChildData  = {this.setChildData} />
-      </div>
-    )
-
-  }
-  setChildData = (data) => {
-    this.setState({
-      title:data
-    })
-  }
-
-}
-
-
-class ChildCom extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      msg:'hello,react'
+    parentEvent = (e,msg) => {
+        //  rea
+      console.log(e);
+      console.log(msg);
+      e.preventDefault();
     }
-  }
-  render(){
-    return (
-      <div>
-        <button onClick = {this.sendData.bind(this)}>点击子组件传值</button>
-      </div>
-    )
-  }
-  sendData(){
-    this.props.setChildData(this.state.msg);
-    this.setState({
-      msg:'111'
-    })
-  }
+    render(){
+        return (
+            <div onClick = {(e) => {this.parentEvent(e,'123')}}>
+                <div className = "child">
+                    <h2>hello,parent component</h2>
+                </div>
+            </div>
+        )
+    }
 }
-
 
 
 
